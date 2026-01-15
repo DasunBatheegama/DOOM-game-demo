ef8520f02b78b19851551978538e6b2505760b49
Dasun Batheegama <dasunbatheegama256@gmail.com>
2026-01-15 09:52:44 +0530

    Add object renderer and wall textures
    Introduced ObjectRenderer class for loading and managing wall textures. Updated main.py to initialize ObjectRenderer, added texture size settings, and fixed a typo in player.py ('drew' to 'draw'). Added five wall texture image files.

--- player.py
+++ player.py
@@ -47,7 +47,7 @@ class Player:
         if self.check_wall(int(self.x), int(self.y + dy)):
             self.y += dy
 
-    def drew(self):
+    def draw(self):
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)                        
 
     def update(self):
